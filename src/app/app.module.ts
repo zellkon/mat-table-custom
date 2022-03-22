cc8d22094e106ca0d062764d9db0dcedfa3a10eb
Nguyen Duy Nguyen <nguyennd@rnt.vn>
2022-03-22 09:51:37 +0700

    update css drag

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from 'src/material.module';
 import { ResizeColumnDirective } from './resizeColumn.directive';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [	
@@ -14,7 +15,8 @@ import { ResizeColumnDirective } from './resizeColumn.directive';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
